7f4e0e55c8884815f6f38102817611b2a0fa8404
carolinavigil <criscarolinava@gmail.com>
2025-07-05 17:49:23 -0500

    add

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Dynamically set the current year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Dynamically set the last modified date of the document
+document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;
